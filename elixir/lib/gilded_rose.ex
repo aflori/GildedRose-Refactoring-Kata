e18e8488cc26dce0877461812ca2102a5efd665a
aflori <aurelien.flori@le-campus-numerique.fr>
2024-03-13 23:33:53 +0100

    my resolution

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -1,83 +1,53 @@
 defmodule GildedRose do
-  # Example
-  # update_quality([%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 1}])
-  # => [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 8, quality: 3}]
+  @aged "Aged Brie"
+  @backstage "Backstage passes to a TAFKAL80ETC concert"
+  @sulfuras "Sulfuras, Hand of Ragnaros"
 
   def update_quality(items) do
     Enum.map(items, &update_item/1)
   end
 
   def update_item(item) do
-    item = cond do
-      item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-        if item.quality > 0 do
-          if item.name != "Sulfuras, Hand of Ragnaros" do
-            %{item | quality: item.quality - 1}
-          else
-            item
-          end
-        else
-          item
-        end
-      true ->
-        cond do
-          item.quality < 50 ->
-            item = %{item | quality: item.quality + 1}
-            cond do
-              item.name == "Backstage passes to a TAFKAL80ETC concert" ->
-                item = cond do
-                  item.sell_in < 11 ->
-                    cond do
-                      item.quality < 50 ->
-                        %{item | quality: item.quality + 1}
-                      true -> item
-                    end
-                  true -> item
-                end
-                cond do
-                  item.sell_in < 6 ->
-                    cond do
-                      item.quality < 50 ->
-                        %{item | quality: item.quality + 1}
-                      true -> item
-                    end
-                  true -> item
-                end
-              true -> item
-            end
-          true -> item
-        end
+    item |> update_quality_by_name_and_selling |> update_selling_by_name |> update_quality_by_selling
+  end
+
+  defp update_quality_by_name_and_selling(item) do
+    case {item.name, item.quality} do
+      {@aged, quality} when quality>=50 ->       item
+      {@backstage, quality} when quality>=50 ->  item
+      {@sulfuras, _} ->                          item
+      {@aged, quality} ->                        %{ item | quality: quality+1 }
+      {@backstage, quality} when quality==49 ->  %{ item | quality: 50}
+      {@backstage, _quality} ->                   update_backstage_name_quantity_by_selling(item)
+      {_other_name, quality} when quality>0 ->   %{item | quality: quality-1}
+      {_other_name, _quality} ->                 item
     end
-    item = cond do
-      item.name != "Sulfuras, Hand of Ragnaros" ->
-        %{item | sell_in: item.sell_in - 1}
-      true -> item
-    end
+  end
+  defp update_backstage_name_quantity_by_selling(item) do
     cond do
-      item.sell_in < 0 ->
-        cond do
-          item.name != "Aged Brie" ->
-            cond do
-              item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-                cond do
-                  item.quality > 0 ->
-                    cond do
-                      item.name != "Sulfuras, Hand of Ragnaros" ->
-                        %{item | quality: item.quality - 1}
-                      true -> item
-                    end
-                  true -> item
-                end
-              true -> %{item | quality: item.quality - item.quality}
-            end
-          true ->
-            cond do
-              item.quality < 50 ->
-                %{item | quality: item.quality + 1}
-              true -> item
-            end
-        end
-      true -> item
+      item.sell_in < 6 ->  %{item | quality: item.quality+3}
+      item.sell_in < 11 -> %{item | quality: item.quality+2}
+      true ->              %{item | quality: item.quality+1}
+    end
+  end
+  defp update_selling_by_name(item) do
+     case item.name do
+         @sulfuras ->  item
+         _name ->      %{item | sell_in: item.sell_in - 1}
+    end
+  end
+
+  defp update_quality_by_selling(item) when item.sell_in >= 0 do
+    item
+  end
+  defp update_quality_by_selling(item) do
+    case {item.name, item.quality} do
+      {@aged, quality} when quality < 50 ->       %{item | quality: quality+1}
+      {@aged, _quality} ->                        item
+      {@backstage, _quality} ->                   %{item | quality: 0}
+      {@sulfuras, _quality} ->                    item
+      {_other_name, quality} when quality > 0 ->  %{item | quality: quality - 1}
+      {_other_name, _quality} ->                  item
     end
   end
 end
