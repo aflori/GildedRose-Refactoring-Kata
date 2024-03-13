8c5043da7aab47f4bc16c9fc90931f57b34eaaaa
aflori <aurelien.flori@le-campus-numerique.fr>
2024-03-13 21:11:23 +0100

    added tests

--- elixir/test/gilded_rose_test.exs
+++ elixir/test/gilded_rose_test.exs
@@ -3,8 +3,22 @@ defmodule GildedRoseTest do
 
   test "begin the journey of refactoring" do
     items = [%Item{name: "foo", sell_in: 0, quality: 0}]
-    GildedRose.update_quality(items)
+    items = GildedRose.update_quality(items)
     %{name: firstItemName} = List.first(items)
     assert "foo" == firstItemName
+  end
+
+  test "empty list" do
+    items = []
+    items = GildedRose.update_quality(items)
+
+    assert [] == items
+  end
+
+  test "foo bar test" do
+    items = [%Item{name: "foo", sell_in: 8, quality: 1}, %Item{name: "bar", sell_in: 3, quality: 0}]
+    items = GildedRose.update_quality(items)
+
+    assert [%Item{name: "foo", quality: 0, sell_in: 7}, %Item{name: "bar", quality: 0, sell_in: 2}] == items
   end
 end
