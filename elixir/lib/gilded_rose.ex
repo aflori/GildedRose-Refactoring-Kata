2c76c8d382741beacc6c94da7cdc8537c9000394
aflori <f.aurelien@yahoo.fr>
2024-04-09 23:14:49 +0200

    refacto #43

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -19,6 +19,8 @@ defmodule GildedRose do
         %{item | quality: quality + 2}
       %Item{quality: quality, name: ^backstage} when quality < 50 ->
         %Item{item | quality: quality + 1}
+      %Item{name: ^backstage} ->
+        item
       %Item{name: name, quality: quality} when name != aged and name != backstage and name != sulfuras and quality > 0 ->
         %{item | quality: quality-1 }
       %Item{name: name} when name != aged and name != backstage ->
