f5ff9325a734992b00648fa9e3633286d0a70af9
aflori <f.aurelien@yahoo.fr>
2024-04-09 23:08:11 +0200

    refacto #39 bis

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -17,14 +17,14 @@ defmodule GildedRose do
         %{item | quality: quality-1 }
       %Item{name: name} when name != aged and name != backstage ->
         item
-          %Item{quality: quality, name: ^backstage, sell_in: sell} when quality < 49 and sell < 6 ->
-            %{item | quality: quality + 3}
-          %Item{quality: quality, name: ^backstage, sell_in: sell} when quality < 49 and sell < 11 ->
-            %{item | quality: quality + 2}
-          %Item{quality: quality} when quality < 50 ->
-            %Item{item | quality: quality + 1}
-          _ ->
-            item
+      %Item{quality: quality, name: ^backstage, sell_in: sell} when quality < 49 and sell < 6 ->
+        %{item | quality: quality + 3}
+      %Item{quality: quality, name: ^backstage, sell_in: sell} when quality < 49 and sell < 11 ->
+        %{item | quality: quality + 2}
+      %Item{quality: quality} when quality < 50 ->
+        %Item{item | quality: quality + 1}
+      _ ->
+        item
     end
     item = cond do
       item.name != "Sulfuras, Hand of Ragnaros" ->
