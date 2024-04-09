7633dcdaed0f313de668c5078ba91c83b81630ba
aflori <f.aurelien@yahoo.fr>
2024-04-10 01:13:09 +0200

    refacto #150

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -26,8 +26,8 @@ defmodule GildedRose do
         case item2 do
           %Item{name: ^backstage, quality: quality2, sell_in: sell2} when sell < 1  and quality < 49 and sell < 11->
             %{item | quality: 0, sell_in: sell - 1}
-          %Item{name: ^backstage, quality: quality2, sell_in: sell2} ->
-            item2
+          %Item{name: ^backstage, quality: quality2, sell_in: sell2} when quality < 49 and sell < 11 ->
+            %Item{item | quality: quality + 2, sell_in: sell - 1}
         end
       %Item{name: ^backstage, quality: quality, sell_in: sell} when sell < 1 and quality < 50 ->
         %Item{item | quality: 0, sell_in: sell - 1}
