bacad2d3f9451d27277e2f81c6d0cc23d767610f
aflori <f.aurelien@yahoo.fr>
2024-04-11 19:40:43 +0200

    refacto #177

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -2,40 +2,39 @@ defmodule GildedRose do
   # Example
   # update_quality([%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 1}])
   # => [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 8, quality: 3}]
+  @aged ConstantName.aged()
+  @backstage ConstantName.backstage()
+  @sulfuras ConstantName.sulfuras()
 
   def update_quality(items) do
     Enum.map(items, &update_item/1)
   end
 
   def update_item(item) do
-    sulfuras = ConstantName.sulfuras
-    backstage = ConstantName.backstage
-    aged = ConstantName.aged
-
     case item do
-      %Item{name: ^backstage, quality: quality, sell_in: sell} when quality < 49 and sell < 1   ->
+      %Item{name: @backstage, quality: quality, sell_in: sell} when quality < 49 and sell < 1   ->
         %Item{item | quality: 0, sell_in: sell - 1}
-      %Item{name: ^backstage, quality: quality, sell_in: sell} when quality < 49 and sell < 6   ->
+      %Item{name: @backstage, quality: quality, sell_in: sell} when quality < 49 and sell < 6   ->
         %Item{item | quality: quality + 3, sell_in: sell - 1}
-      %Item{name: ^backstage, quality: quality, sell_in: sell} when quality < 49 and sell < 11  ->
+      %Item{name: @backstage, quality: quality, sell_in: sell} when quality < 49 and sell < 11  ->
         %Item{item | quality: quality + 2, sell_in: sell - 1}
-      %Item{name: ^backstage, quality: quality, sell_in: sell} when quality < 50 and sell < 1   ->
+      %Item{name: @backstage, quality: quality, sell_in: sell} when quality < 50 and sell < 1   ->
         %Item{item | quality: 0, sell_in: sell - 1}
-      %Item{name: ^backstage, quality: quality, sell_in: sell} when quality < 50                ->
+      %Item{name: @backstage, quality: quality, sell_in: sell} when quality < 50                ->
         %Item{item | quality: quality + 1, sell_in: sell - 1}
-      %Item{name: ^backstage, sell_in: sell}                   when sell < 1                    ->
+      %Item{name: @backstage, sell_in: sell}                   when sell < 1                    ->
         %Item{item | quality: 0, sell_in: sell-1}
-      %Item{name: ^backstage, sell_in: sell}                                                    ->
+      %Item{name: @backstage, sell_in: sell}                                                    ->
         %Item{item | sell_in: sell - 1}
 
-      %Item{name: ^aged, quality: quality, sell_in: sell}      when quality < 49 and sell < 1   ->
+      %Item{name: @aged, quality: quality, sell_in: sell}      when quality < 49 and sell < 1   ->
         %{item | quality: quality + 2, sell_in: item.sell_in - 1}
-      %Item{name: ^aged, quality: quality}                     when quality < 50                ->
+      %Item{name: @aged, quality: quality}                     when quality < 50                ->
         %Item{item | quality: quality + 1, sell_in: item.sell_in - 1}
-      %Item{name: ^aged}                                                                        ->
+      %Item{name: @aged}                                                                        ->
         %Item{item | sell_in: item.sell_in - 1}
 
-      %Item{name: ^sulfuras}                                                                    ->
+      %Item{name: @sulfuras}                                                                    ->
         item
 
       %Item{quality: quality, sell_in: sell}                   when quality > 1 and sell < 1    ->
