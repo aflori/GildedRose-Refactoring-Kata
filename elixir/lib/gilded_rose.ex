f305b1cdc5c28e9b073b2c21f2cdc32854416ece
aflori <f.aurelien@yahoo.fr>
2024-04-12 17:12:49 +0200

    refacto #199.

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -27,14 +27,8 @@ defmodule GildedRose do
   end
 
   def update_item(item) do
-    cond do
-      item.quality > 1 and item.sell_in < 1                     ->
-        %Item{item | quality: item.quality - 2 }
-      item.quality > 0                                          ->
-        %Item{item | quality: item.quality - 1 }
-      true                                                      ->
-        item
-    end
+    item 
+      |> update_standard_quality
       |> remove_a_seller
   end
 
@@ -45,34 +39,46 @@ defmodule GildedRose do
 
   defp update_backstage_quality(item) do
     cond do
-      item.sell_in < 1                            ->
+      item.sell_in < 1                           ->
         %Item{item | quality: 0}
 
-      item.quality > 49                           ->
+      item.quality > 49                          ->
         item
 
-      item.quality == 49 or item.sell_in >= 11    ->
+      item.quality == 49 or item.sell_in >= 11   ->
         %Item{item | quality: item.quality + 1}
 
       item.sell_in < 6                           ->
         %Item{item | quality: item.quality + 3}
 
-      true                                        ->
+      true                                       ->
         %Item{item | quality: item.quality + 2}
     end
   end
 
   defp update_aged_quality(item) do
     cond do
-      item.quality < 49 and item.sell_in < 1                    ->
+      item.quality < 49 and item.sell_in < 1     ->
         %{item | quality: item.quality + 2}
 
-      item.quality < 50                                         ->
+      item.quality < 50                          ->
         %Item{item | quality: item.quality + 1}
 
-      true                             ->
+      true                                       ->
         item
     end
   end
 
+  defp update_standard_quality(item) do
+    cond do
+      item.quality > 1 and item.sell_in < 1      ->
+        %Item{item | quality: item.quality - 2 }
+
+      item.quality > 0                           ->
+        %Item{item | quality: item.quality - 1 }
+
+      true                                       ->
+        item
+    end
+  end
 end
