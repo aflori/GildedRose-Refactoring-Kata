1bf50034c901c41ad5ac41a9991866943c908106
aflori <f.aurelien@yahoo.fr>
2025-03-03 20:45:28 +0100

    refacto 84

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -16,23 +16,20 @@ defmodule GildedRose do
   @spec update_item(%Item{}) :: %Item{}
   def update_item(item) do
     item = case item do
-          %Item{name: @aged, quality: quality} when quality <= 49 ->
-            %{item | quality: quality + 1}
-          %Item{name: @aged} ->
-            item
-      %Item{name: @backstage} ->
-        case item do
-          %Item{name: @backstage, quality: quality} when quality == 49 ->
-            %{item | quality: item.quality + 1}
-          %Item{name: @backstage, sell_in: sell, quality: quality} when quality < 49 and sell < 6 ->
-            %{item | quality: quality + 3}
-          %Item{name: @backstage, sell_in: sell, quality: quality} when quality < 49 and sell < 11 ->
-            %{item | quality: quality + 2}
-          %Item{name: @backstage, quality: quality} when quality < 49->
-            %{item | quality: quality + 1}
-          %Item{name: @backstage} ->
-            item
-        end
+        %Item{name: @aged, quality: quality} when quality <= 49 ->
+          %{item | quality: quality + 1}
+        %Item{name: @aged} ->
+          item
+        %Item{name: @backstage, quality: quality} when quality == 49 ->
+          %{item | quality: item.quality + 1}
+        %Item{name: @backstage, sell_in: sell, quality: quality} when quality < 49 and sell < 6 ->
+          %{item | quality: quality + 3}
+        %Item{name: @backstage, sell_in: sell, quality: quality} when quality < 49 and sell < 11 ->
+          %{item | quality: quality + 2}
+        %Item{name: @backstage, quality: quality} when quality < 49->
+          %{item | quality: quality + 1}
+        %Item{name: @backstage} ->
+          item
         %Item{} ->
           case item do
             %Item{quality: quality} when quality <= 0 ->
