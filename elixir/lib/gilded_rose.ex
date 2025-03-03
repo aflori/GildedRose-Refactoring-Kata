916a678fc069a0bc60748a4b197fe0b290cd48a4
aflori <f.aurelien@yahoo.fr>
2025-03-03 22:28:56 +0100

    refacto 156

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -31,12 +31,13 @@ defmodule GildedRose do
     end
     case item do
       %Item{sell_in: sell}                  when (sell - 1) >= 0    ->
-        %Item{item | sell_in: item.sell_in - 1}
+        item
       %Item{quality: quality}               when quality <= 0 ->
-        %Item{item | sell_in: item.sell_in - 1}
+        item
       %Item{}                                                 ->
-        %{item | quality: item.quality - 1, sell_in: item.sell_in - 1}
+        %{item | quality: item.quality - 1}
     end
+    |> remove_a_sell()
   end
 
   @spec update_quality_of_named_item(%Item{}) :: %Item{}
