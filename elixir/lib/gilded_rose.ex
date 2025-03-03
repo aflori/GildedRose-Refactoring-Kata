f828dd663f268c12793c0ee3da6ac4b2db4218f9
aflori <f.aurelien@yahoo.fr>
2025-03-03 20:59:08 +0100

    refacto 94

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -28,7 +28,7 @@ defmodule GildedRose do
     cond do
       item.sell_in >= 0 ->
         item
-      item.quality < 50 ->
+      item.quality < 50 and item.sell_in < 0->
         %{item | quality: item.quality + 1}
       true ->
         item
